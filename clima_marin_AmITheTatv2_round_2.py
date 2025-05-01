82057c917292831260afb3ca0bc50b535b133b2c
CmMarin <57499487+CmMarin@users.noreply.github.com>
2025-05-01 17:21:09 +0300

    Update clima_marin_AmITheTatv2_round_2.py
    - Tournament legal

--- clima_marin_AmITheTatv2_round_2.py
+++ clima_marin_AmITheTatv2_round_2.py
@@ -1,39 +1,28 @@
-blacklist = set()
-
 def strategy_round_2(opponent_id: int, my_history: dict[int, list[int]], opponents_history: dict[int, list[int]]) -> tuple[int, int]:
-    global blacklist
-
     rounds_played = len(my_history[opponent_id])
     opponent_moves = opponents_history[opponent_id]
 
-    if 0 in opponent_moves:
-        blacklist.add(opponent_id)
+    is_blacklisted = 0 in opponent_moves
 
     if rounds_played + 1 == 200:
-        move = 0
+        move = 0  
+    elif rounds_played == 0:
+        move = 1
     else:
-        if rounds_played == 0:
-            move = 1
-        else:
-            if opponent_id in blacklist:
-                move = 0
-            else:
-                move = opponent_moves[-1]
+        move = 0 if is_blacklisted else opponent_moves[-1]  
 
-    next_opponent = opponent_id
+    next_opponent = opponent_id 
 
-    if opponent_id in blacklist:
+    if is_blacklisted:
         best_choice = None
         best_score = -1
 
         for player, history in opponents_history.items():
-            if player in blacklist:
-                continue
-            if len(history) >= 200:
-                continue
+            if 0 in history or len(history) >= 200:
+                continue 
 
             if not history:
-                score = 2.0
+                score = 2.0 
             else:
                 coop_rate = history.count(1) / len(history)
                 score = coop_rate
